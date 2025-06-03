88901112b6a3d75588fa839186838ca97c60f715
Luke Johnson <mcmhusker@gmail.com>
2025-06-03 18:29:22 -0500

    Tweaks to test map
    Added a few new obstacles to mess around on; updated VoidArea so that objects now teleport to (0, 5, 0) rather than (0, 0, 0)

--- scripts/void_area.gd
+++ scripts/void_area.gd
@@ -3,4 +3,4 @@ extends Area3D
 
 # reset any objects that fall back to the top of the map
 func _on_body_entered(body: Node3D) -> void:
-	body.global_position = Vector3.ZERO
+	body.global_position = Vector3(0, 5, 0)
