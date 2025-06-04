45cfb624aa55ada8644d03c44b2695ab413b0274
Silicontent <mcmhusker@gmail.com>
2025-06-03 22:36:52 -0500

    Small updates to HUD & test map
    Replaced Panel reticle & circle with TextureRect reticle & circle; added simple reticle circle animation using lerp(); added more obstacles to map; updated way ReachRay detects player

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,7 +3,9 @@ extends CanvasLayer
 @export var player: Player
 
 @onready var pause_menu := $Pause
-@onready var interact_circle := $Overlay/InteractCircle
+@onready var reticle_circle := $Overlay/ReticleCircle
+
+const RETICLE_ANIM_SPEED := 0.25
 
 
 func _process(_delta: float) -> void:
@@ -12,6 +14,13 @@ func _process(_delta: float) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		get_tree().paused = true
 	
-	# DEBUG: show a circle around the reticle if the player is looking at
-	# and can reach an object
-	interact_circle.visible = player.reach_ray.is_colliding()
+	update_reticle()
+
+
+func update_reticle() -> void:
+	var current_scale := float(player.reach_ray.get_collider() is PhysicsBody3D)
+	
+	reticle_circle.scale = Vector2(
+		lerp(reticle_circle.scale.x, current_scale, RETICLE_ANIM_SPEED),
+		lerp(reticle_circle.scale.y, current_scale, RETICLE_ANIM_SPEED)
+	)
